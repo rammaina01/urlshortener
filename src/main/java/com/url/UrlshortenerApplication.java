3a78606f19288c062a507f1ac5dbff505f6eb386
H343428 <rambabu.maina@honeywell.com>
2021-05-17 18:24:29 +0530

    Adding docker compose file

--- src/main/java/com/url/UrlshortenerApplication.java
+++ src/main/java/com/url/UrlshortenerApplication.java
@@ -2,10 +2,12 @@ package com.url;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication()
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
